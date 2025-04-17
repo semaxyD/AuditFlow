c79f89e7e8509ecd9166e5d8785f1d068eca425d
semaxyD <88868446+semaxyD@users.noreply.github.com>
2025-04-17 17:38:20 -0500

    Update user.module.ts

--- 02-backend/src/components/User-management/user/user.module.ts
+++ 02-backend/src/components/User-management/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from '@data-access/src/prisma/prisma.service';
+import { DatabaseModule } from '@data-access/database.module'; 
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
   providers: [UserService, PrismaService],
   exports: [UserService],
