c9c2586c79fc62d96519b357d1fc48e8559b4d5d
edwingd18 <edwin.guerrero@uao.edu.co>
2025-04-24 23:31:28 -0500

    fix: correct companyByUser query logic

--- 03-data-access/src/components/system-schema/queries/user-queries.ts
+++ 03-data-access/src/components/system-schema/queries/user-queries.ts
@@ -1,51 +1,56 @@
-import { prisma } from '../../../prismaconfig/prisma-client'
+import { prisma } from "../../../prismaconfig/prisma-client";
 
 //Query para hu002 - ver todo los usuarios actuales del sistema
 export async function searchUser() {
-    const userFind = await prisma.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        role: true,
-      },
-    });
-    return userFind;
+  const userFind = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    },
+  });
+  return userFind;
 }
 
 //Query para hu002 - Buscar un usaurio especifico para poder ver su informaicon
 export async function getUserById(id: number) {
-    const userById = await prisma.user.findUnique({
-        where: { id },
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          role: true,
-        },
-    });
-    return userById;
+  const userById = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    },
+  });
+  return userById;
 }
 
 //Query para hu001-005 - Buscar un usaurio existente con el correo proporcionado
 export async function getUserByEmail(email: string) {
-    const userByEmail = await prisma.user.findUnique({
-        where: { email },
-    });
-    return userByEmail;
+  const userByEmail = await prisma.user.findUnique({
+    where: { email },
+  });
+  return userByEmail;
 }
 
 //Query para hu001 - Crear un usuario apartir de sus datos proporcionados
-export async function createUser(data: { name: string, email: string, password: string, role: string }) {
-    const userCreate = await prisma.user.create({ data });
-    return userCreate;
+export async function createUser(data: {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}) {
+  const userCreate = await prisma.user.create({ data });
+  return userCreate;
 }
 
 //Query para hu008 - Buscar el id de la compañia a la cual el user esta asociado
 export async function getUserCompanyById(userId: number) {
-    const user = await prisma.user.findUnique({
-      where: { id : userId },
-      select: { id: true, company_id: true },
-    });
-    return user;
-  }
+  const editor = await prisma.companyEditor.findMany({
+    where: { user_id: userId },
+    select: { company_id: true },
+  });
+  return editor;
+}
