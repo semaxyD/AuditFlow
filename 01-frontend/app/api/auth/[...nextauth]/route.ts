9b0245c77374e0738671057cb40db3252b5acdb4
edwingd18 <edwin.guerrero@uao.edu.co>
2025-05-08 00:33:19 -0500

    al fin

--- 01-frontend/app/api/auth/[...nextauth]/route.ts
+++ 01-frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -10,34 +11,45 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        try {
-          // Aquí se implementará la llamada al backend para autenticación
-          const response = await fetch("http://localhost:3001/user/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(credentials),
-          });
-
-          const user = await response.json();
-
-          if (response.ok && user) {
-            return user;
-          }
-          return null;
-        } catch (error) {
-          console.error("Error de autenticación:", error);
-          return null;
+        const res = await fetch("http://localhost:3001/user/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        });
+        const user = await res.json();
+        if (res.ok && user.access_token) {
+          return {
+            id: user.id,
+            email: user.email,
+            accessToken: user.access_token,
+          };
         }
+        return null;
       },
     }),
   ],
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log("user", user);
+      if (user) {
+        token.accessToken = user.accessToken;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+
   pages: {
     signIn: "/login",
     newUser: "/register",
   },
-  session: {
-    strategy: "jwt",
-  },
-});
+};
 
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
